0b16e57ed8835f723733f641338692a1da8bbe1d
Dhruv4171 <110565663+Dhruv4171@users.noreply.github.com>
2024-09-15 12:23:59 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score = 33
+let score1 = "33abc"
+let score2 = null
+let score3 = undefined
+let score4 = true
+
+// console.log(typeof score) => number
+// console.log(typeof(score)) => number
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) => number
+// console.log(valueInNumber)=> 33
+
+// console.log(typeof score1) => string
+// console.log(typeof(score1)) => string
+
+// let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber); => number
+// console.log(valueInNumber); => NaN Not a Number
+
+// console.log(typeof score2) => object
+// console.log(typeof(score2)) => object
+
+// let valueInNumber = Number(score2)
+// console.log(typeof valueInNumber); => number
+// console.log(valueInNumber); => 0
+
+// console.log(typeof score3) => undefined
+// console.log(typeof(score3)) => undefined
+ 
+// let valueInNumber = Number(score3)
+// console.log(typeof valueInNumber); =>number
+// console.log(valueInNumber); => NaN
+
+// console.log(typeof score4) => boolean
+// console.log(typeof(score4)) => boolean
+
+// let valueInNumber = Number(score4)
+// console.log(typeof valueInNumber); => number
+// console.log(valueInNumber); ture => 1 false => 0
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 & false =>0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = "hello"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false; 
+//"hello" => true
+
+// let someNumber = 33
+// let stringSomeNumber = String(someNumber)
+// console.log(stringSomeNumber) => 33
+// console.log(typeof stringSomeNumber) => string
+
+
+
