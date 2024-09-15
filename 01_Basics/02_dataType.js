564c6b2055cfb04ff389c0a3c82eda6520aba674
Dhruv4171 <110565663+Dhruv4171@users.noreply.github.com>
2024-09-15 11:32:33 +0000

    datatypes

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+// console.log(3 
+//     + 
+//     3); // this practice is not right there should be code readability
+
+let name = "dhruv"
+let age = 18 
+let isLoggedIn = false
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value representation of empty value
+// undefined =>
+// symbol => unique value
+
+// object 
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(isLoggedIn))// => boolean
+console.log(typeof(undefined))// => undefined
+console.log(typeof(null))// => object
+
