9eee9b2091d73815db34109274b565326e7b37c4
Dhruv4171 <parwanidhruv0990@gmail.com>
2024-09-16 01:42:32 +0530

    datatypes

--- 01_Basics/05_datatypes2.js
+++ 01_Basics/05_datatypes2.js
@@ -1,6 +1,43 @@
 //Primitive Datatype
 // 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt 
 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+// console.log(typeof(score)) => number
+// console.log(typeof(scoreValue)) => number
+// console.log(typeof(isLoggedIn)) => boolean
+// console.log(typeof(outsideTemp)) => object
+// console.log(typeof(userEmail)) => undefined
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+// console.log(typeof(id)) => Symbol
+// console.log(id === anotherID) => false
+
+const bigNumber = 345654634557634565n
+// console.log(typeof(bigNumber)) => bigint
+
+
 // Dynamically Typed Language
 // Reference (Non primitive)
 // Array , Object , Function 
+
+const heros = ['Thor', 'Hulk' , "Wanda"]
+let myObj = {
+    name : "Ironman",
+    age : 42
+}
+
+const myFunction = function(){
+    console.log("first function")
+}
+
+// console.log(typeof heros) => Object
+// console.log(typeof myObj) => Object
+// console.log(typeof myFunction)=> function Object
